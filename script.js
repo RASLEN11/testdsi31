0fa627bdaf63ef39f482eb5fa7eb9bf2a583d7aa
RASLEN11 <115932709+RASLEN11@users.noreply.github.com>
2025-10-20 10:11:59 +0100

    Add functions to illustrate variable scope
    Demonstrate variable scope using var, let, and const.

---
+++ script.js
@@ -0,0 +1,11 @@
+function myFunction1() {
+  var carName = "Volvo";  // Function Scope
+}
+
+function myFunction2() {
+  let carName = "Volvo";  // Function Scope
+}
+
+function myFunction3() {
+  const carName = "Volvo";  // Function Scope
+}
